957c0a3c47346857d16211a82516dc07233eef54
paradise <xkcocco@yahoo.com>
2021-06-05 12:47:16 +0100

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,30 @@
+function getElementImage(element) {
+  switch(element) {
+    case "pyro":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/e/e8/Element_Pyro.png";
+    case "hydro":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/3/35/Element_Hydro.png";
+    case "dendro":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/f/f4/Element_Dendro.png";
+    case "electro":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/7/73/Element_Electro.png";
+    case "anemo":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/a/a4/Element_Anemo.png";
+    case "cryo":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/8/88/Element_Cryo.png";
+    case "geo":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/4/4a/Element_Geo.png";
+    case "sword":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Sword.png";
+    case "claymore":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/6/66/Icon_Claymore.png";
+    case "polearm":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/6/6a/Icon_Polearm.png";
+    case "catalyst":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/2/27/Icon_Catalyst.png";
+    case "bow":
+      return "https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Bow.png";
+    default:
+      return undefined;
+  }
+}
